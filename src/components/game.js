57e0e774cb75905575f710e7efc9ffe6367d20eb
Janet Leon <leon.irenejanet@gmail.com>
2018-10-04 10:11:16 -0700

    caputuring value

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,37 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
+class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            randomNum: Math.floor(Math.random()*101),
+            guessCount: 0,
+            guessList: []
+        }  
+    }
+
+
+
+  render(){
+    console.log('hello');
+
     return (
         <div>
             <Header />
-            <GuessSection feedback="Make your guess!" />
+            <GuessSection InputValue={e=>this.input=e} submit={e=>{
+                e.preventDefault();
+                console.log(this.input.value);
+                console.log("I'm Not submitting");
+               }}/>
             <GuessCount count={3} />
             <GuessList guesses={[10, 15, 25]} />
         </div>
     );
+
+  }
 }
+
+
+  export default Game
 
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 import './guess-form.css';
 
-export default function GuessForm(props) {
-    return (
-        <form>
-            <input type="text" name="userGuess" id="userGuess"
-                className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
-            <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
-        </form>
-    );
-};
+export default function GuessForm (props){
+  
+        return (
+            <form onSubmit={props.submit}>
+                <input type="text" name="userGuess" id="userGuess"
+                    className="text" maxLength="3" autoComplete="off"
+                    placeholder="Enter your Guess" required ref={props.InputValue}/>
+    
+                <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+            </form>
+        );
+    }
+
+
+
 
